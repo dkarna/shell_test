ce5161e08efb3b7a4d993c5a611d4f3534e39096
dkarna <karna.deepak@gmail.com>
2016-03-14 04:17:58 +0000

    Some more additions on reference programs

--- salesbonus.sh
+++ salesbonus.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-declare -a salesperson
-declare -a salary
+declare -a salesperson #=("a" "b" "c" "d" "e")
+declare -a salary #=(1 2 3 4 5)
 
-i=0
+#i=0
 
 for((i=0; i <= 5; i++))
-	echo "Salesperson: "
-	read salesperson[i]
-	echo "salary: "
-	read salary[i]
+do
+	echo "Salesperson $i: "
+	read salesperson[$i]
+	echo "Quaterly sales $i: "
+	read salary[$i]
 
+done
 
+echo "Here are your values:"
+for((i=0; i< ${#salesperson[@]}; i++))
+do
+	echo $i
+	echo "Salesperson $i: "
+	echo "${salesperson[$i]}"
+	echo "Quaterly sales $i: "
+	echo "${salary[$i]}"
+
+done
